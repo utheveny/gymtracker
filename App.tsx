91a84b3625f3f555729fb0d870a41da3f30d5cc2
S-i-d-V <me@ugotheveny.dev>
2023-11-27 23:58:17 +0100

    ✨ Training List

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MyTrainingScreen from './MyTrainingScreen';
 import MyDatasScreen from './MyDatasScreen';
--- MyTrainingScreen.tsx
+++ MyTrainingScreen.tsx
@@ -1,10 +1,176 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  Button,
+  Modal,
+  TextInput,
+  StyleSheet,
+} from "react-native";
 
 export default function MyTrainingScreen() {
+  // List
+  const [sessions, setSessions] = useState<string[]>([]);
+
+  //Create Modal related
+  const [isCreateModalVisible, setCreateModalVisible] =
+    useState<boolean>(false);
+  const [newSession, setNewSession] = useState<string>("");
+
+  //Edit Modal related
+  const [isEditModalVisible, setEditModalVisible] = useState<boolean>(false);
+  const [editingIndex, setEditingIndex] = useState<number | null>(null);
+  const [editedSession, setEditedSession] = useState<string>("");
+
+  const handleAddSession = () => {
+    if (newSession.trim() !== "") {
+      setSessions([...sessions, newSession]);
+      setNewSession("");
+      setCreateModalVisible(false);
+    }
+  };
+
+  const handleEditSession = (index: number, session: string) => {
+    setEditingIndex(index);
+    setEditedSession(session);
+    setEditModalVisible(true);
+  };
+
+  const handleEditSessionConfirm = () => {
+    if (editingIndex !== null) {
+      const updatedSessions = [...sessions];
+      updatedSessions[editingIndex] = editedSession;
+      setSessions(updatedSessions);
+      setEditingIndex(null);
+    }
+    setEditedSession("");
+    setEditModalVisible(false);
+  };
+
+  const handleDeleteSession = (index: number) => {
+    const updatedSessions = [...sessions];
+    updatedSessions.splice(index, 1);
+    setSessions(updatedSessions);
+  };
+
   return (
     <View>
-      <Text>Mon Programme</Text>
+      <Text>Mes Programmes</Text>
+      <Text>Mes séances :</Text>
+      <FlatList
+        data={sessions}
+        keyExtractor={(index) => index.toString()}
+        renderItem={({ index, item }) => {
+          return (
+            <View style={styles.listItemContainer}>
+              <Text style={styles.listItemText}>{item}</Text>
+              <View style={styles.listItemButtons}>
+                <Button
+                  title="Modifier"
+                  onPress={() => handleEditSession(index, item)}
+                />
+                <Button
+                  title="Supprimer"
+                  onPress={() => handleDeleteSession(index)}
+                />
+              </View>
+            </View>
+          );
+        }}
+      />
+
+      {/* Add a Session */}
+      <Button
+        title="Ajouter une séance"
+        onPress={() => setCreateModalVisible(true)}
+      />
+
+      {/* Create Modal */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={isCreateModalVisible}
+        onRequestClose={() => setCreateModalVisible(false)}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text>Nouvelle Séance</Text>
+            <TextInput
+              placeholder="Nom de la séance"
+              onChangeText={(text) => setNewSession(text)}
+              value={newSession}
+            />
+            <Button title="Ajouter" onPress={handleAddSession} />
+            <Button
+              title="Annuler"
+              onPress={() => {
+                setNewSession("");
+                setEditedSession("");
+                setCreateModalVisible(false);
+              }}
+            />
+          </View>
+        </View>
+      </Modal>
+
+      {/* Edit Modal */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={isEditModalVisible}
+        onRequestClose={() => setEditModalVisible(false)}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text>Modifier Séance</Text>
+            <TextInput
+              placeholder="Nom de la séance"
+              onChangeText={(text) => setEditedSession(text)}
+              value={editedSession}
+            />
+            <Button title="Modifier" onPress={handleEditSessionConfirm} />
+            <Button
+              title="Annuler"
+              onPress={() => {
+                setNewSession("");
+                setEditedSession("");
+                setEditModalVisible(false);
+              }}
+            />
+          </View>
+        </View>
+      </Modal>
     </View>
   );
-};
+}
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)", // Fond semi-transparent
+  },
+  modalContent: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
+    width: "80%",
+  },
+  listItemContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+  },
+  listItemText: {
+    flex: 1,
+    marginRight: 10,
+  },
+  listItemButtons: {
+    flexDirection: "row",
+  },
+});
