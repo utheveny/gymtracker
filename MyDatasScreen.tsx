620b1be25f10598d62815ed8128254bc9cba8f82
S-i-d-V <me@ugotheveny.dev>
2023-11-27 23:51:33 +0100

    🎨 Format component declaration

--- MyDatasScreen.tsx
+++ MyDatasScreen.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const MyDatasScreen: React.FC = () => {
+export default function MyDatasScreen() {
   return (
     <View>
       <Text>Mes donnees</Text>
     </View>
   );
 };
-
-export default MyDatasScreen;
--- MyTrainingScreen.tsx
+++ MyTrainingScreen.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const MyTrainingScreen: React.FC = () => {
+export default function MyTrainingScreen() {
   return (
     <View>
       <Text>Mon Programme</Text>
     </View>
   );
 };
-
-export default MyTrainingScreen;
