6f80a6ebbe9a33bda265b429e1a4e3451887dbf2
S-i-d-V <me@ugotheveny.dev>
2023-11-28 23:57:28 +0100

    ✨ Can now add exercices to session

--- screens/MyTraining/MyTrainingScreen.tsx
+++ screens/MyTraining/MyTrainingScreen.tsx
@@ -1,18 +1,15 @@
 import React, { useState } from "react";
-import {
-  View,
-  Text,
-  FlatList,
-  Button,
-  Modal,
-  TextInput,
-  StyleSheet,
-} from "react-native";
-import { styles } from './styles';
+import { View, Text, FlatList, Button, Modal, TextInput } from "react-native";
+import { styles } from "./styles";
+
+interface Session {
+  name: string;
+  exercises: string[];
+}
 
 export default function MyTrainingScreen() {
   // List
-  const [sessions, setSessions] = useState<string[]>([]);
+  const [sessions, setSessions] = useState<Session[]>([]);
 
   //Create Modal related
   const [isCreateModalVisible, setCreateModalVisible] =
@@ -23,25 +20,34 @@ export default function MyTrainingScreen() {
   const [isEditModalVisible, setEditModalVisible] = useState<boolean>(false);
   const [editingIndex, setEditingIndex] = useState<number | null>(null);
   const [editedSession, setEditedSession] = useState<string>("");
+  const [editedExercises, setEditedExercises] = useState<string[]>([]);
+  const [isAddExerciseModalVisible, setAddExerciseModalVisible] =
+    useState<boolean>(false);
+  const [newExercise, setNewExercise] = useState<string>("");
 
   const handleAddSession = () => {
     if (newSession.trim() !== "") {
-      setSessions([...sessions, newSession]);
+      const newSessionObject: Session = {
+        name: newSession,
+        exercises: [],
+      };
+      setSessions([...sessions, newSessionObject]);
       setNewSession("");
       setCreateModalVisible(false);
     }
   };
 
-  const handleEditSession = (index: number, session: string) => {
+  const handleEditSession = (index: number, session: Session) => {
     setEditingIndex(index);
-    setEditedSession(session);
+    setEditedSession(session.name);
+    setEditedExercises(session.exercises);
     setEditModalVisible(true);
   };
 
   const handleEditSessionConfirm = () => {
     if (editingIndex !== null) {
       const updatedSessions = [...sessions];
-      updatedSessions[editingIndex] = editedSession;
+      updatedSessions[editingIndex].name = editedSession;
       setSessions(updatedSessions);
       setEditingIndex(null);
     }
@@ -55,6 +61,24 @@ export default function MyTrainingScreen() {
     setSessions(updatedSessions);
   };
 
+  const handleOpenAddExerciseModal = () => {
+    setAddExerciseModalVisible(true);
+  };
+
+  const handleAddExercise = () => {
+    if (newExercise.trim() !== "") {
+      setEditedExercises([...editedExercises, newExercise]);
+      setNewExercise("");
+      setAddExerciseModalVisible(false);
+    }
+  };
+
+  const handleDeleteExercise = (index: number) => {
+    const updatedExercises = [...editedExercises];
+    updatedExercises.splice(index, 1);
+    setEditedExercises(updatedExercises);
+  };
+
   return (
     <View style={styles.container}>
       <Text>Mes Programmes</Text>
@@ -65,12 +89,13 @@ export default function MyTrainingScreen() {
         renderItem={({ index, item }) => {
           return (
             <View style={styles.listItemContainer}>
-              <Text style={styles.listItemText}>{item}</Text>
+              <Text
+                style={styles.listItemText}
+                onPress={() => handleEditSession(index, item)}
+              >
+                {item.name}
+              </Text>
               <View style={styles.listItemButtons}>
-                <Button
-                  title="Modifier"
-                  onPress={() => handleEditSession(index, item)}
-                />
                 <Button
                   title="Supprimer"
                   onPress={() => handleDeleteSession(index)}
@@ -130,6 +155,27 @@ export default function MyTrainingScreen() {
               onChangeText={(text) => setEditedSession(text)}
               value={editedSession}
             />
+
+            {/* Exerices */}
+            <Text>Exercices :</Text>
+            <FlatList
+              data={editedExercises}
+              keyExtractor={(item, index) => index.toString()}
+              renderItem={({ item, index }) => (
+                <View style={styles.listItemContainer}>
+                  <Text style={styles.listItemText}>{item}</Text>
+                  <Button
+                    title="Supprimer"
+                    onPress={() => handleOpenAddExerciseModal()}
+                  />
+                </View>
+              )}
+            />
+            <Button
+              title="Ajouter un exercice"
+              onPress={() => setAddExerciseModalVisible(true)}
+            />
+
             <Button title="Modifier" onPress={handleEditSessionConfirm} />
             <Button
               title="Annuler"
@@ -137,6 +183,33 @@ export default function MyTrainingScreen() {
                 setNewSession("");
                 setEditedSession("");
                 setEditModalVisible(false);
+              }}
+            />
+          </View>
+        </View>
+      </Modal>
+
+      {/* Add exercice modal */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={isAddExerciseModalVisible}
+        onRequestClose={() => setAddExerciseModalVisible(false)}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text>Ajouter un exercice</Text>
+            <TextInput
+              placeholder="Nom de l'exercice"
+              onChangeText={(text) => setNewExercise(text)}
+              value={newExercise}
+            />
+            <Button title="Ajouter" onPress={handleAddExercise} />
+            <Button
+              title="Annuler"
+              onPress={() => {
+                setNewExercise("");
+                setAddExerciseModalVisible(false);
               }}
             />
           </View>
